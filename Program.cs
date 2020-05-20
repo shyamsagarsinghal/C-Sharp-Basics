ecd09d493870182a5020eb364df72bc81ec44aa5
Singhal <singhals@avaya.com>
2020-05-20 22:45:22 +0530

    Intial Project

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter First Name: ");
+        string FirstName = Console.ReadLine();
+
+        Console.Write("Enter Second Name: ");
+        string LastName = Console.ReadLine();
+
+        Console.WriteLine("Hello " + FirstName + " " + LastName); // Concatenation
+        Console.WriteLine("Hello {0} {1}", FirstName, LastName); // Place Holder Syntax
+        Console.WriteLine($"Hello {FirstName} {LastName}");     // Latest One
+        Console.ReadLine();
+
+        //Note 1: A namespace is used to organize your code and is collection of classes, interfaces, structs, enums, namespace and delegates.
+        //Note 2: To write to Console, there are three Types as mentioned above
+        //Note 3: C# is a case sensitive Language
+    }
+}
+
