7a3bf81cdbe3375764c559c2449ec3e48f482080
Singhal <singhals@avaya.com>
2020-05-22 10:46:49 +0530

    Built-in Types

--- Program.cs
+++ Program.cs
@@ -17,6 +17,28 @@ class Program
         //Note 1: A namespace is used to organize your code and is collection of classes, interfaces, structs, enums, namespace and delegates.
         //Note 2: To write to Console, there are three Types as mentioned above
         //Note 3: C# is a case sensitive Language
+
+        //Boolean Types - true or false
+
+        //Integral Types - sbyte, byte, short, ushort, int, unit, long, ulong, char
+        Console.WriteLine("Integer Min Value: {0}", int.MinValue);
+        Console.WriteLine("Integer Max Value: {0}", int.MaxValue); // using this we can know what is the minimum and maximum value of the particular data type
+
+        Console.WriteLine();
+
+        //Floating Types - float and double
+        Console.WriteLine("Float Min Value: {0}", float.MinValue);
+        Console.WriteLine("Double Max Value: {0}", double.MaxValue);
+
+        Console.WriteLine();
+
+        //Decimal Types
+        Console.WriteLine("Decimal Min Value: {0}", decimal.MinValue);
+        Console.WriteLine("Decimal Max Value: {0}", decimal.MaxValue);
+
+        //String Type
+        string Name = " \"Shyam\" ";   //This is called Escape Sequences.. There are many types of Escape Sequences
+        Console.WriteLine(Name);
     }
 }
 
